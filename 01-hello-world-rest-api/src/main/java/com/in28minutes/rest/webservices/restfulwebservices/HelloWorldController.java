c60b7d9d001b9d6e9b3fc2911724878585b60cc0
Alekya1990 <p.alekya30@gmail.com>
2025-03-23 07:59:23 +0530

    added message

--- 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World Alerag1";
+		return "Hello World raghavendra";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
