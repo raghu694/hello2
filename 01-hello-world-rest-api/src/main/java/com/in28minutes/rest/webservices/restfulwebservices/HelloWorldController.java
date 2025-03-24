2c322ab54bc41bf874887c28c8f61e1d9120042a
raghu694 <33100843+raghu694@users.noreply.github.com>
2025-03-24 08:35:28 +0530

    Updated HelloWorldController.java

--- 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ 01-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World Alerag";
+		return "Hello World Alerag1";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
